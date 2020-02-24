75ed276dc3266eb8d9bca0bf7d9979b6c4c5ac3c
shishir75 <shishir.srdr16@gmail.com>
2020-02-24 23:42:12 +0600

    StatusController with route

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,5 @@ Route::get('/', function () {
 });
 
 
-//Route::get('/{any}', function () {
-//    return view('welcome');
-//})->where('any', '.*');
-
-
-Route::get('/statuses', function () {
-
-    return \App\Status::with('user')->latest()->get();
-
-});
+Route::get('/statuses', 'StatusController@index');
+Route::post('/statuses', 'StatusController@store');
